5be7a1f46c798216096d62b92730005d04fd9fdf
shamim159 <shamin_ali@hotmail.co.uk>
2024-06-26 18:04:19 +0100

    created the full functionality on console

--- main.js
+++ main.js
@@ -12,7 +12,16 @@ const divide = function(n, i) {
   return n / i;
 }
 
-const getInputOne = prompt("please select a number");
+const num1 = parseInt(prompt("please select a number"));
 const operator = prompt("please select an operator");
-const getInputTwo = prompt("please select the second number");
+const num2 = parseInt(prompt("please select the second number"));
 
+if (operator == "+"){
+  console.log(add(num1, num2))
+}else if(operator =="-"){
+  console.log(subtract(num1, num2))
+}else if(operator == "*"){
+  console.log(multiply(num1, num2))
+}else if(operator == "/"){
+  console.log(divide(num1, num2))
+};
