66a992d759e80ccb89099ff4658725f9264f6d54
shamim159 <shamin_ali@hotmail.co.uk>
2024-06-26 13:18:37 +0100

    arithmetic functions created

---
+++ main.js
@@ -0,0 +1,14 @@
+const add = function(n, i) {
+  return n+i;
+};
+const subtract = function(n, i) {
+	return n-i
+};
+const multiply = function(n, i) {
+  return  n * i;
+};
+const divide = function(n, i) {
+  return n / i;
+}
+
+console.log(divide(6,2))
